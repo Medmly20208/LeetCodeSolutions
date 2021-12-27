8d7e0952d2706ea6d283c6329b95737d7e4943bd
Medmly20208 <moulaymohamed856@gmail.com>
2021-12-27 07:54:30 +0100

    FinalValue Of Variable After Performing Operations

---
+++ FinalValueOfVariableAfterPerformingOperations.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+ var finalValueAfterOperations = function(operations) {
+    let x = 0;
+    for(let i = 0;i<operations.length;i++){
+        if(operations[i].includes("+")){
+            x+=1
+        }
+        else{
+            x-=1
+        }
+        
+    }
+    return x
+    
+};
